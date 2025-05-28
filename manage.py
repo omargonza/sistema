a8d43d69ee5c59a55ab7b4c8d20a718a45df44a0
omargonza <gonzamartinez1081@gmail.com>
2025-05-28 10:09:54 -0300

    config  sistema_ordenes

--- manage.py
+++ manage.py
@@ -11,9 +11,9 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "No se pudo importar Django. "
+            "¿Estás seguro de que está instalado y disponible en la variable de entorno PYTHONPATH? "
+            "¿Olvidaste activar un entorno virtual?"
         ) from exc
     execute_from_command_line(sys.argv)
 
