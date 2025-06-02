77dc6b3a3cf2b34622fd75c97ab7390336739918
omargonza <gonzamartinez1081@gmail.com>
2025-06-02 15:13:23 -0300

    Ajuste

--- sistema_ordenes/settings.py
+++ sistema_ordenes/settings.py
@@ -133,3 +133,19 @@ LOGOUT_REDIRECT_URL = '/accounts/login/' # URL a la que quieres redirigir despu�
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+from django.contrib.auth import get_user_model
+
+if os.environ.get('CREATE_SUPERUSER', 'false') == 'true':
+    User = get_user_model()
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print("✅ Superusuario creado automáticamente.")
+    else:
+        print("ℹ️ El superusuario ya existe.")
