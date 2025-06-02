2c7f68886ae93929f75e330dec9be6b762900297
omargonza <gonzamartinez1081@gmail.com>
2025-06-02 09:11:20 -0300

    Ajuste

--- sistema_ordenes/settings/prod.py
+++ sistema_ordenes/settings/prod.py
@@ -7,10 +7,21 @@ DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
-# Configuración base de datos usando DATABASE_URL
-DATABASES = {
-    'default': db_url(config('DATABASE_URL'))
-}
+DATABASE_URL = config('DATABASE_URL', default='')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': db_url(DATABASE_URL)
+    }
+    print("⚙️ Usando base de datos externa:", DATABASE_URL)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+    print("⚙️ Usando base de datos local SQLite")
 
 # Seguridad para producción
 SECURE_SSL_REDIRECT = True
