871e420e97ed7d9731ebc0eaa0d67fdc3fef0255
omargonza <gonzamartinez1081@gmail.com>
2025-06-02 14:31:43 -0300

    Ajuste

--- sistema_ordenes/settings.py
+++ sistema_ordenes/settings.py
@@ -1,6 +1,7 @@
 # sistema_ordenes/settings.py
 
 from pathlib import Path
+import dj_database_url
 import os # Necesario para os.path.join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -72,14 +73,9 @@ WSGI_APPLICATION = 'sistema_ordenes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
